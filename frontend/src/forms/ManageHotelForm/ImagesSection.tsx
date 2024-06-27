8a87c2cfa64bd5803712d1255d46b10c77f88ae1
Abhias2405 <iamabhisheksingh56@gmail.com>
2024-06-27 22:34:00 +0530

    add edit hotel functionality and e2e test

--- frontend/src/forms/ManageHotelForm/ImagesSection.tsx
+++ frontend/src/forms/ManageHotelForm/ImagesSection.tsx
@@ -5,12 +5,47 @@ const ImagesSection = () => {
   const {
     register,
     formState: { errors },
+    watch,
+    setValue,
   } = useFormContext<HotelFormData>();
+
+  const existingImageUrls = watch("imageUrls");
+
+  const handleDelete = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    imageUrl: string
+  ) => {
+    event.preventDefault();
+    setValue(
+      "imageUrls",
+      existingImageUrls.filter((url) => url !== imageUrl)
+    );
+  };
 
   return (
     <div className="bg-primary-100 p-6 rounded-lg shadow-lg">
       <h2 className="text-2xl font-bold text-secondary mb-4">Images</h2>
-      <div className="border border-secondary-300 rounded-lg p-4 bg-primary-200">
+      <div className="border border-secondary-300 rounded-lg p-4 bg-primary-200 flex flex-col gap-4">
+        {existingImageUrls && (
+          <div className="grid grid-cols-6 gap-4">
+            {existingImageUrls.map((url) => (
+              <div key={url} className="relative group">
+                <img
+                  src={url}
+                  alt="Uploaded preview"
+                  className="min-h-full object-cover rounded-lg border border-secondary-300"
+                />
+                <button
+                  onClick={(event) => handleDelete(event, url)}
+                  className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 text-white font-bold rounded-lg"
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
+
         <input
           type="file"
           multiple
@@ -18,7 +53,8 @@ const ImagesSection = () => {
           className="w-full text-secondary bg-primary-200 py-2 px-3 rounded-lg border border-secondary-300 focus:outline-none focus:ring-2 focus:ring-accent"
           {...register("imageFiles", {
             validate: (imageFiles) => {
-              const totalLength = imageFiles.length;
+              const totalLength =
+                imageFiles.length + (existingImageUrls?.length || 0);
 
               if (totalLength === 0) {
                 return "At least one image should be added";
